88fc9847b9e7743f34ae962df1cca98c1d29627b
Bruno44rus <56883210+Bruno44rus@users.noreply.github.com>
2025-10-25 11:50:25 +0300

    Create App.js

---
+++ 6.docker-multistage-volumes/1.node-multistage/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "3rem"}}>
+      <h1>Hello from Dockerized React!</h1>
+      <p>This app was built inside a container 🚀</p>
+    </div>
+  );
+}
+
+export default App;
